46d2b6ef76e33b44113bc5145d6b0f28cbee0da8
franksutom <franksu@FrankdeMacBook-Pro.local>
2017-11-27 19:58:23 +0800

    2017.11.27.a
    优化程序

--- TN_LED_Ctrl.ino
+++ TN_LED_Ctrl.ino
@@ -1,35 +1,25 @@
 
+/*
+TN的红外感应器触发动态led灯带和播放视频
+接收："video_stop"，"video_play"
+发送："play"，"stop"
+功能：没触摸时视频时待机界面，当触发红外感应器时启动动态灯带和播放视频，播放视频当中被触摸
+     不会执行，播放视频结束后回到待机界面
+问题：arduino的启动加载会有一个检测后再加载的过程，所以不能开机后就发指令
+ */
+ 
 #include <SoftwareSerial.h> 
 
 #define LEDWallPinRx 8  //墙面LED模块接收端
 #define LEDWallPinTx 7  //墙面LED模块发射端
 
-#define Sensor 10  //红外LED模块发射端
+int Sensor = 10;  //定义触发红外感应器模块接收端的针脚为10
 
-#define TimeoutMax 300000 //超时时间
+String cmd = "stop" ;
+String VedioState = "video_stop";
 
-//mp3 模块软连接
-SoftwareSerial CinemaDoorModel(CinemaDoorPinRx, CinemaDoorPinTx); // RX, TX
+//LED 模块软连接
 SoftwareSerial LEDWallModel(LEDWallPinRx, LEDWallPinTx); // RX, TX
-SoftwareSerial LEDGroundModel(LEDGroundPinRx, LEDGroundPinTx); // RX, TX
-
-//影院入口门开命令
-byte CinemaEnterOpen[] = {0xFD,0x03,0xEB,0xDD,0x72,0x6C,0xDF};
-
-//影院入口门关命令
-byte CinemaEnterClose[] = {0xFD,0x03,0xEB,0xDD,0x78,0x6B,0xDF};
-
-//影院入口门感应开关命令
-byte CinemaEnterAuto[] = {0xFD,0x03,0xEB,0xDD,0x71,0x6D,0xDF};
-
-//影院出口门开命令
-byte CinemaExitOpen[] = {0xFD,0x03,0x0F,0x55,0x72,0x6D,0xDF};
-
-//影院出口门关命令
-byte CinemaExitClose[] = {0xFD,0x03,0x0F,0x55,0x71,0x6D,0xDF};
-
-//影院出口门感应开关命令
-byte CinemaExitAuto[] = {0xFD,0x03,0x0F,0x55,0x78,0x6D,0xDF};
 
 //播放LED跑动命令
 byte playLED[] = {0xAA,0x26};
@@ -37,96 +27,57 @@ byte playLED[] = {0xAA,0x26};
 //停止播放LED跑动命令
 byte stopLED[] = {0xAA,0x62};
 
-//设置状态
-boolean onFlag=false;
-
 void setup() {
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
-  CinemaDoorModel.begin(9600);
   LEDWallModel.begin(9600);
-  LEDGroundModel.begin(9600);
+  pinMode(Sensor,INPUT);
 }
 
 void loop() {
 
-  String cmd = readCmd();
+  // 读取触发针脚的状态
+  int buttonState ;
+  String VedioTempState = "";
 
-  Serial.print(cmd);
-
-  if(cmd.startsWith("Run"))
+  // 读取上位机播放视频的状态
+  if(Serial.available() > 0)
   {
-    LEDWallModel.write(playLED,2);
-    delay(3000);
-    LEDGroundModel.write(playLED,2);
-    onFlag= true;
-    Serial.println(" ok");
-  }
-  else if(cmd.startsWith("Stop"))
-  {
-    LEDWallModel.write(stopLED,2);
-    delay(50);
-    LEDGroundModel.write(stopLED,2);
-    onFlag= true;
-    Serial.println(" ok");
-  }
-  else if(cmd.startsWith("OpenEnterDoor"))
-  {
-    LEDWallModel.write(CinemaEnterOpen,7);
-    onFlag= true;
-    Serial.println(" ok");
-  }
-  else if(cmd.startsWith("CloseEnterDoor"))
-  {
-    LEDWallModel.write(CinemaEnterClose,7);
-    onFlag= true;
-    Serial.println(" ok");
-  }
-  else if(cmd.startsWith("AutoEnterDoor"))
-  {
-    LEDWallModel.write(CinemaEnterAuto,7);
-    onFlag= true;
-    Serial.println(" ok");
-  }
-  else if(cmd.startsWith("OpenExitDoor"))
-  {
-    LEDWallModel.write(CinemaExitOpen,7);
-    onFlag= true;
-    Serial.println(" ok");
-  }
-  else if(cmd.startsWith("CloseExitDoor"))
-  {
-    LEDWallModel.write(CinemaExitClose,7);
-    onFlag= true;
-    Serial.println(" ok");
-  }
-  else if(cmd.startsWith("AutoExitDoor"))
-  {
-    LEDWallModel.write(CinemaExitAuto,7);
-    onFlag= true;
-    Serial.println(" ok");
-  }
- 
-  delay(50);
-}
-
-//读取指令操作
-String readCmd(){
-   boolean stringComplete=false;
-   String inputString="";
-   //Serial.listen();
-    delay(50);
-   while(Serial.available()>0 && stringComplete==false){
-          char inChar = (char)Serial.read();
-          if (inChar == '\n') {
-              stringComplete = true;
-          }else{
-              inputString += inChar;               
-          }
+    while (Serial.available() > 0) 
+    {
+      VedioTempState += char(Serial.read());
+      delay(2);
     }
-    return inputString;
+    VedioState = VedioTempState;
+  }
+  //Serial.println("vedio:"+VedioState+" ");
+  
+  // 判断上位机是否在播放视频，如果播放视频就让视频播完
+  // 如果没有播放视频，有触发感应器就发送播放视频指令，没触发就发送停止视频指令
+  if(VedioState == "video_stop")
+  {
+    buttonState = digitalRead(Sensor);
+    if((buttonState == HIGH)&&(cmd == "stop")) //被触发后发播放命令
+    {
+      cmd = "play";
+      LEDWallModel.write(playLED,2);
+      Serial.println(cmd);
+    }
+      
+   if((buttonState == LOW)&&(cmd == "play"))  //没有触发后发停止命令
+   {
+      cmd = "stop";
+      LEDWallModel.write(stopLED,2);
+      Serial.println(cmd);
+   }
+      
+    //Serial.println((String)i+" oldcmd:"+oldcmd+" cmd:"+cmd);
+  }
+
+  delay(50);        // delay in between reads for stability
+
 }
 
